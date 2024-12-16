de5d9206d0748f406ba54ac30089de325e7c495a
Anmol Shrestha <anmolshres98@users.noreply.github.com>
2024-12-16 17:23:12 -0600

    fix possibly

--- src/index.tsx
+++ src/index.tsx
@@ -28,6 +28,20 @@ if (!process.env.IMJS_AUTH_CLIENT_REDIRECT_URI) {
   );
 }
 
+if (window.location.host.includes('localhost') || window.location.host.includes('dev.')) {
+  (globalThis as any).IMJS_BUDDI_RESOLVE_URL_USING_REGION = 102;
+
+  (globalThis as any).IMJS_URL_PREFIX = 'qa-';
+} else if (window.location.host.includes('qa.')) {
+  (globalThis as any).IMJS_BUDDI_RESOLVE_URL_USING_REGION = 102;
+
+  (globalThis as any).IMJS_URL_PREFIX = 'qa';
+} else {
+  (globalThis as any).IMJS_BUDDI_RESOLVE_URL_USING_REGION = 0;
+
+  (globalThis as any).IMJS_URL_PREFIX = '';
+}
+
 Auth.initialize({
   scope: process.env.IMJS_AUTH_CLIENT_SCOPES,
   clientId: process.env.IMJS_AUTH_CLIENT_CLIENT_ID,
